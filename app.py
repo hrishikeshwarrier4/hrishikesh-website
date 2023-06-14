b34ae06f094a9f39634b5eb8e059d8821ffe0890
hrishikeshwarrier4 <warrier.h@northeastern.edu>
2023-06-14 15:19:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada",layout="wide")
+
+
+# Header
+
+st.subheader("Hi, I am Hrishikesh Warrier :wave:")
+st.title("A Data Engineer and Data Analyst from Boston")
+
+
+
